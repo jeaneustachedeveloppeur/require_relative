e43d48cd17c1f940d15c289e5fbb8bd4fd8a88bc
jeaneustachedeveloppeur <95184639+jeaneustachedeveloppeur@users.noreply.github.com>
2023-01-19 10:19:44 +0100

    Add files via upload

---
+++ lib/maths.rb
@@ -0,0 +1,13 @@
+require_relative "./constant.rb"
+
+def sum(a, b)
+  a + b
+end
+
+def multiply(a, b)
+  a * b
+end
+
+def add_number_in_the_constant_file(number)
+  CONSTANT + number
+end
---
+++ main.rb
@@ -0,0 +1,7 @@
+require_relative "./lib/maths.rb"
+
+puts sum(1, 2)
+
+puts multiply(7, 8)
+
+puts add_number_in_the_constant_file(7)
